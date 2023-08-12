fefff5b87d29013f9640c72c42e23ac9384cdebc
YUUKKY <43734044+YUUKKY@users.noreply.github.com>
2023-08-12 08:58:35 +0800

    Update Application.java for best practice writing

--- src/main/java/com/huawei/cae/Application.java
+++ src/main/java/com/huawei/cae/Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
+        Thread.sleep(60000);
         SpringApplication.run(Application.class,args);
     }
 }
